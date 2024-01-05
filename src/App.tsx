74c01314ad543d797b9e532a4baf40f6b24e5279
AbraaoAlves <abraao.teodosio@gmail.com>
2024-01-05 11:36:13 -0300

    feat(PizzaScreen): logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
-import './App.css'
-import { Header } from './components/Header'
-import { ImgClient } from './components/ImgClient'
-import { OpcoesMassa } from './components/OpcoesMassa'
-import { OpcoesMolho } from './components/OpcoesMolho'
-import { OpcoesTipo } from './components/OpcoesTipo'
-import { PizzaScreen } from './components/PizzaScreen'
+import { useState } from "react";
+
+import "./App.css";
+import { Header } from "./components/Header";
+import { ImgClient } from "./components/ImgClient";
+import { OpcoesMassa } from "./components/OpcoesMassa";
+import { OpcoesMolho } from "./components/OpcoesMolho";
+import { OpcoesTipo } from "./components/OpcoesTipo";
+import { PizzaScreen } from "./components/PizzaScreen";
+
+const initState = { 
+  borda: "fina", 
+  molho: "amarelo", 
+  tipo: ["verdura", "carne"] 
+};
 
 function App() {
+  const [state, _setState] = useState(initState);
+
+  // setState({ borda: "", molho: "", tipo: [] }); //certa
 
   return (
     <>
-        
-  <div id="screenMaker">
-      <Header />
+      {/* <div id="screenMaker">
+        <Header />
 
-      <div className="row">
-        <OpcoesMassa/>
-        <OpcoesMolho/>
-      </div>
-      <div className="row">
-        <OpcoesTipo/>
-       <ImgClient />
-      </div>
-      <footer>
-        <button className="hidden"> ➡️ </button>
-      </footer>
-    </div>
+        <div className="row">
+          <OpcoesMassa />
+          <OpcoesMolho />
+        </div>
+        <div className="row">
+          <OpcoesTipo />
+          <ImgClient />
+        </div>
+        <footer>
+          <button className="hidden"> ➡️ </button>
+        </footer>
+      </div> */}
 
-<PizzaScreen/>
+      <PizzaScreen molho={state.molho} borda={state.borda} tipos={state.tipo} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/PizzaScreen.jsx
+++ src/components/PizzaScreen.jsx
@@ -1,45 +1,70 @@
 
-export function PizzaScreen() {
+/**
+ * @typedef {Object} Props
+ * @property {string} Props.molho - pode ser `vermelho` ou `amarelo`
+ * @property {string} Props.borda - pode ser `borda`, `tradicional` ou `fina`
+ * @property {Array<string>} Props.tipos - pode ser `carne` ou `verdura` 
+ * 
+ * @param {Props} props 
+ * @returns 
+ */
+export function PizzaScreen({ molho, tipos, borda }) {
+  let bordaTipo = '';
+
+  switch (borda) {
+    case 'borda':
+      bordaTipo = 'Pizzaa-01';
+    case 'tradicional':
+      bordaTipo = 'Pizzaa-02';
+    case 'fina':
+      bordaTipo = 'Pizzaa-03';
+  }
+
+  const bordaUrl = `https://studio.code.org/v3/assets/1M0wVT7H--jMKxjDJMc3ZQZGs7pYH3KRZNxtxFT3m3U/${bordaTipo}.png?t=1670932317000`
+  
+  const molhoUrl = 
+    molho === 'amarelo' ? 
+    `https://studio.code.org/v3/assets/1M0wVT7H--jMKxjDJMc3ZQZGs7pYH3KRZNxtxFT3m3U/Pizzaa-04.png?t=1670932317000`
+    : `https://studio.code.org/v3/assets/1M0wVT7H--jMKxjDJMc3ZQZGs7pYH3KRZNxtxFT3m3U/Pizzaa-05.png?t=1670932317000`
+  
+  let tipoUrl;
+
+  if (tipos.length === 2) {
+    tipoUrl = 'https://studio.code.org/v3/assets/ai36I_gY5JPKznE1ttg-CqVl0NhRRSPu1b34n7Ctx6c/Non-veg.png?t=1671892191000'
+  } else {
+    if (tipos[0] === 'carne'){
+      tipoUrl = 'https://studio.code.org/v3/assets/1M0wVT7H--jMKxjDJMc3ZQZGs7pYH3KRZNxtxFT3m3U/veg-nonveg.png?t=1670932317000'
+    }
+    if (tipos[0] === 'verdura'){
+      tipoUrl = 'https://studio.code.org/v3/assets/1M0wVT7H--jMKxjDJMc3ZQZGs7pYH3KRZNxtxFT3m3U/Veg.png?t=1670932317000'
+    }
+  } 
+
+
+
+
   return (
-    <section id="screenPizza" className="hidden">
+    <section id="screenPizza">
       <div id="imagens">
         <img
           id="imageBase"
-          src="https://studio.code.org/v3/assets/1M0wVT7H--jMKxjDJMc3ZQZGs7pYH3KRZNxtxFT3m3U/Pizzaa-03.png?t=1670932317000"
+          src={bordaUrl}
           alt="massa"
         />
 
         <img
-          className="hidden"
-          id="imageYellowSauce"
-          src="https://studio.code.org/v3/assets/1M0wVT7H--jMKxjDJMc3ZQZGs7pYH3KRZNxtxFT3m3U/Pizzaa-04.png?t=1670932317000"
-          alt="molho amarelo"
-        />
-        <img
-          className="hidden"
-          id="imageRedSauce"
-          src="https://studio.code.org/v3/assets/1M0wVT7H--jMKxjDJMc3ZQZGs7pYH3KRZNxtxFT3m3U/Pizzaa-05.png?t=1670932317000"
-          alt="molho vermelho"
+          src={molhoUrl}
+          alt="molho"
         />
 
-        <img
-          className="hidden"
-          id="imageVegNon-veg"
-          src="https://studio.code.org/v3/assets/1M0wVT7H--jMKxjDJMc3ZQZGs7pYH3KRZNxtxFT3m3U/veg-nonveg.png?t=1670932317000"
-          alt="Non-Veg"
-        />
-        <img
-          className="hidden"
-          id="imageVeg"
-          src="https://studio.code.org/v3/assets/1M0wVT7H--jMKxjDJMc3ZQZGs7pYH3KRZNxtxFT3m3U/Veg.png?t=1670932317000"
-          alt="Non-V /eg"
-        />
-        <img
-          className="hidden"
-          id="imageNon-veg"
-          src="https://studio.code.org/v3/assets/ai36I_gY5JPKznE1ttg-CqVl0NhRRSPu1b34n7Ctx6c/Non-veg.png?t=1671892191000"
-          alt="Non-Veg"
-        />
+        {tipoUrl && 
+        
+          <img
+            src={tipoUrl}
+            alt="recheio"
+          />
+        }
+        
       </div>
       <footer>
         <button id="buttonBack">⬅️</button>
