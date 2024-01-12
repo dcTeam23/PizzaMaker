3b6e6a62f8b2213108873f288eb64e5f486feae9
AbraaoAlves <abraao.teodosio@gmail.com>
2024-01-12 11:01:59 -0300

    feat: sync Borda

--- src/components/MakerScreen.jsx
+++ src/components/MakerScreen.jsx
@@ -5,14 +5,35 @@ import { OpcoesMolho } from "./OpcoesMolho";
 import { OpcoesTipo } from "./OpcoesTipo";
 
 export function MakerScreen({submit, onChange}) {
+  const changeBorda = (value) => {
+    onChange((atual) => ({...atual, borda: value}));
+  };
+
+  const changeMolho = () => {
+    const molho = 'vermelho';
+    onChange((atual) => ({...atual, molho: molho}));
+  };
+
+  const changeTipo = () => {
+    const tipo = ['carne'];
+    onChange((atual) => ({...atual, tipo: tipo}));
+  };
+
+  
   return <div id="screenMaker">
     <Header />
 
     <div className="row">
-      <OpcoesMassa />
+      {/* `onChange(
+            (s) => ({...s, massa: 'vermelha'})
+          )`
+       quando a Massa for escolhida */}
+      <OpcoesMassa onMassa={changeBorda} />
+      {/* `onChange()` quando o Molho for escolhido */}
       <OpcoesMolho />
     </div>
     <div className="row">
+      {/* `onChange()` quando o Tipo for escolhido */}
       <OpcoesTipo />
       <ImgClient />
     </div>
--- src/components/OpcoesMassa.jsx
+++ src/components/OpcoesMassa.jsx
@@ -1,22 +1,28 @@
-export function OpcoesMassa() {
+export function OpcoesMassa({onMassa}) {
   
   return (
     <section>
       <h2>Massa:</h2>
       <ul>
-        <OpcaoMassa id="1" value="borda"/>
-        <OpcaoMassa id="2" value="tradicional" />
-        <OpcaoMassa id="3" value="fina" />
+        <OpcaoMassa onCheck={() => onMassa('borda')} id="1" value="borda"/>
+        <OpcaoMassa onCheck={() => onMassa('tradicional')} id="2" value="tradicional" />
+        <OpcaoMassa onCheck={() => onMassa('fina')} id="3" value="fina" />
       </ul>
     </section>
   );
 }
 
-function OpcaoMassa({ id, value }) {
+function OpcaoMassa({ id, value, onCheck }) {
   const url = `https://studio.code.org/v3/assets/1M0wVT7H--jMKxjDJMc3ZQZGs7pYH3KRZNxtxFT3m3U/Pizzaa-0${id}.png?t=1670932317000`;
   return (
     <li>
-      <input type="radio" id={"radioBase" + id} name="massa" value={value} />
+      <input 
+          type="radio"
+           id={"radioBase" + id} 
+           name="massa" 
+          value={value}
+          onClick={onCheck} 
+      />
       <label htmlFor={"radioBase" + id}>
         <img className="img-icon" src={url} alt={value} />
       </label>
