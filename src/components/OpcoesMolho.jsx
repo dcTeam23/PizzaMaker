7657f68f7f8d0822bfa9fba551bc6641cecc6de1
AbraaoAlves <abraao.teodosio@gmail.com>
2024-01-19 12:51:39 -0300

    feat: handle state change in Molho component

--- src/components/OpcoesMolho.jsx
+++ src/components/OpcoesMolho.jsx
@@ -1,25 +1,46 @@
-export function OpcoesMolho() {
+/**
+ * @typedef {import('../state').State['molho']} Molho
+ *
+ *
+ * @param {{onMolho: (s: Molho) => void}} param0
+ * @returns
+ */
+export function OpcoesMolho({ onMolho }) {
+  
+  const handleChange = ({target: {value}}) => {
+    onMolho(value);
+  }  
+
   return (
     <section>
       <h2>Molho:</h2>
       <ul>
-        <Opcao id="5" value="vermelho"/>
-        <Opcao id="6" value="amarelo"/>
+        <Opcao id="5" value="vermelho" onChange={handleChange} />
+        <Opcao id="4" value="amarelo" onChange={handleChange} />
       </ul>
     </section>
   );
 }
 
+/**
+ * 
+ * @param {{id: string, value: 'vermelho'|'amarelo', onChange}} props
+ */
+function Opcao({ id, value, onChange }) {
+  const url = `https://studio.code.org/v3/assets/1M0wVT7H--jMKxjDJMc3ZQZGs7pYH3KRZNxtxFT3m3U/Pizzaa-0${id}.png?t=1670932317000`;
 
-function Opcao({id, value}){
-  const url = `https://studio.code.org/v3/assets/1M0wVT7H--jMKxjDJMc3ZQZGs7pYH3KRZNxtxFT3m3U/Pizzaa-0${id}.png?t=1670932317000`
-  
   return (
     <li>
-    <input type="radio" id={"radio" + value} name="molho" value={value} />
-    <label htmlFor={"radio" + value}>
-      <img className="img-icon" src={url} alt={"molho " + value} />
-    </label>
-  </li>
-  )
+      <input
+        onChange={onChange}
+        type="radio"
+        id={"radio" + value}
+        name="molho"
+        value={value}
+      />
+      <label htmlFor={"radio" + value}>
+        <img className="img-icon" src={url} alt={"molho " + value} />
+      </label>
+    </li>
+  );
 }
